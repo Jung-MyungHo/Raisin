7ac10e503cac8741c1549aaf361acf7a7f20a97c
jung.mh <jung.mh@raisin-j.com>
2018-04-25 15:07:40 +0900

    Regist master

---
+++ src/main/java/co/jp/raisin/action/RegisterAction.java
@@ -0,0 +1,12 @@
+package co.jp.raisin.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+
+  @Override
+  public String execute() {
+    return SUCCESS;
+  }
+
+}
