cfa331238a91754f825cccd8f86b84a8a7d899af
Jung MyungHo <jung.mh@raisin-j.com>
2018-05-01 16:48:20 +0900

    nav 追加

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -20,14 +20,46 @@
 
   $(function() {
 
+	$("#header").load("/raisin/common/header.html", function(response, status, xhr) {
+		// Off-Canvas Navigation.
+
+	    // Title Bar.
+	    $(
+	      '<div id="titleBar">' +
+	        '<a href="#navPanel" class="toggle"></a>' +
+	        '<span class="title">' + $('#logo').html() + '</span>' +
+	      '</div>'
+	    ).appendTo($body);
+
+	    // Navigation Panel.
+	    $(
+	      '<div id="navPanel">' +
+	        '<nav>' +
+	         $('#nav').navList() +
+	        '</nav>' +
+	      '</div>'
+	    ).appendTo($body).panel({
+	      delay: 500,
+	      hideOnClick: true,
+	      hideOnSwipe: true,
+	      resetScroll: true,
+	      resetForms: true,
+	      side: 'left',
+	      target: $body,
+	      visibleClass: 'navPanel-visible'
+	    });
+
+	    // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+	    if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+	    $('#titleBar, #navPanel, #page-wrapper').css('transition', 'none');
+	});
+	$("#copyright").load("/raisin/common/footer.html");
+
     var $window = $(window),
     $body = $('body');
 
     // Fix: Placeholder polyfill.
     $('form').placeholder();
-
-    $("#header").load("/raisin/common/header.html");
-    $("#copyright").load("/raisin/common/footer.html");
 
     // Prioritize "important" elements on mobile.
     skel.on('+mobile -mobile', function() {
@@ -36,38 +68,6 @@
         skel.breakpoint('mobile').active
       );
     });
-
-    // Off-Canvas Navigation.
-
-    // Title Bar.
-    $(
-      '<div id="titleBar">' +
-        '<a href="#navPanel" class="toggle"></a>' +
-        '<span class="title">' + $('#logo').html() + '</span>' +
-      '</div>'
-    ).appendTo($body);
-
-    // Navigation Panel.
-    $(
-      '<div id="navPanel">' +
-        '<nav>' +
-         $('#nav').navList() +
-        '</nav>' +
-      '</div>'
-    ).appendTo($body).panel({
-      delay: 500,
-      hideOnClick: true,
-      hideOnSwipe: true,
-      resetScroll: true,
-      resetForms: true,
-      side: 'left',
-      target: $body,
-      visibleClass: 'navPanel-visible'
-    });
-
-    // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-    if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-    $('#titleBar, #navPanel, #page-wrapper').css('transition', 'none');
 
   });
 
